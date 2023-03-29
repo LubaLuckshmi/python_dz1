f6a4f1dd2bfafa86bbf7717f17964d10c671ef7a
Lubaluckshmi <lubov.loskutova@mail.ru>
2023-03-29 14:30:46 +0300

    Добавили правки зад 26

--- task26.py
+++ task26.py
@@ -22,3 +22,19 @@ def group(s):
 input_str = ["eat", "tea", "tan", "ate", "nat", "bat", "qwer", "rewq", "wate", "tawe", "awet"]
 print(input_str)
 group(input_str)
+
+'''
+def group_letter(input_list):
+word_dict = {}
+for word in input_list:
+if (set(word), len(word)) not in word_dict:
+word_dict[(set(word), len(word))] = [word]
+else:
+word_dict[(set(word), len(word))].append(word)
+res_list = []
+for value in word_dict.values():
+res_list.append(value)
+return res_list
+
+print(group_letter(["eat", "tea", "tan", "ate", "nat", "bat", 'batt', 'b', 'cadfsdf']))
+'''
